58b3eeeccdb9912fef2f7fd7b11e1e4fb4bf96f6
nerik <erik.escoffier@gmail.com>
2019-09-05 18:54:51 +0200

    WIP proto height map generator

---
+++ server/index.js
@@ -0,0 +1,16 @@
+var path = require('path')
+var express = require('express')
+var app = express()
+var cors = require('cors')
+
+app.use(cors())
+app.use(express.static(path.join(__dirname, '../tiles'), {
+  // TODO Put back for PBF tiles
+  // setHeaders: (res) => {
+  //   // console.log(res)
+  //   res.set('Content-Encoding', 'gzip')
+  // },
+  // fallthrough: false
+}))
+
+app.listen(9090)
