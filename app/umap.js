7594792443722187155bfaffd2fd6f783691ef88
nerik <erik.escoffier@gmail.com>
2019-10-01 16:04:58 +0200

    failed attempt at double pass clustering

--- app/umap.js
+++ app/umap.js
@@ -1,4 +1,4 @@
-/* global d3, Supercluster */
+/* global d3, Supercluster, turf, _ */
 
 var margin = {top: 10, right: 30, bottom: 30, left: 60},
   width = 2000 - margin.left - margin.right,
@@ -26,6 +26,160 @@ document.getElementById('controls').addEventListener('mousedown', e => {
 var url = new URL(window.location)
 var umap = url.searchParams.get('umap') || 'UMAP_with_author'
 
+const getClustersSupercluster = (features, bbox) => {
+  const initialIndex = new Supercluster({
+    radius: .05,
+    maxZoom: 18
+  })
+  initialIndex.load(features)
+  const initialClustersWithNoise = initialIndex.getClusters(bbox, 2)
+  const initialClusters = initialClustersWithNoise
+    // .filter(c => c.properties.cluster_id)
+    .map(cluster => {
+      const initialClusterId = cluster.properties.cluster_id
+      if (initialClusterId === undefined) {
+        return {
+          ...cluster,
+          initialLeaves: [],
+          properties: {
+            ...cluster.properties
+          }
+        }
+      }
+      return {
+        ...cluster,
+        initialLeaves: initialIndex.getLeaves(cluster.properties.cluster_id, Infinity).map(l => l.properties.id),
+        properties: {
+          initialClusterId: cluster.properties.cluster_id,
+          cluster_id: undefined,
+        }
+      }
+    })
+
+  console.log('Initial pass: generated ', initialClusters.length,
+             ' clusters')
+  console.log('Initial pass mean pts per cluster: ', d3.mean(initialClusters.map(c => c.initialLeaves.length)))
+
+  const metaIndex = new Supercluster({
+    radius: .1,
+    maxZoom: 18
+  })
+  metaIndex.load(initialClusters)
+  const metaClustersWithNoise = metaIndex.getClusters(bbox, 2)
+  // const metaClustersWithoutAllLeafClusters = metaClustersWithNoise.filter(metaCluster => {
+  //   const metaClusterId = metaCluster.properties.cluster_id
+  //   if (metaClusterId === undefined) {
+  //     const initialClusterId = metaCluster.properties.initialClusterId
+  //     // case where cluster failed at both level
+  //     if (initialClusterId === undefined) {
+  //       return false
+  //     }
+  //     return true
+  //   }
+  //   const allInitialLeaves = metaIndex.getLeaves(metaClusterId, Infinity)
+  //     .filter(l => l.initialLeaves.length)
+  //   return allInitialLeaves.length
+
+
+  // })
+  const metaClusters = metaClustersWithNoise.map(metaCluster => {
+    const metaClusterId = metaCluster.properties.cluster_id
+    if (metaClusterId === undefined) {
+      const initialClusterId = metaCluster.properties.initialClusterId
+      if (initialClusterId === undefined) {
+        // console.log('standalone point in metacluster standalone point')
+        return {
+          leaves: []
+        }
+      }
+      return {
+        cluster_id: initialClusterId,
+        leaves: initialClusters.find(c => c.properties.initialClusterId === initialClusterId).initialLeaves
+      }
+    }
+    const allInitialLeaves = metaIndex.getLeaves(metaClusterId, Infinity)
+      .map(metaLeaf => {
+        const initialClusterId = metaLeaf.properties.initialClusterId
+        if (initialClusterId === undefined) {
+          return [metaLeaf.properties.id]
+        }
+        const firstCluster = initialClusters.find(c => c.properties.initialClusterId === initialClusterId)
+        return firstCluster.initialLeaves
+      })
+    return {
+      cluster_id: metaClusterId,
+      leaves: _.flatten(allInitialLeaves)
+    }
+  })
+  console.log('Meta pass: generated ', metaClusters.length,
+             ' clusters + ', initialClustersWithNoise.length - initialClusters.length, ' standalone pts')
+  console.log('First pass mean pts per cluster: ', d3.mean(metaClusters.map(c => c.leaves.length)))
+
+
+
+  const featuresDict = {}
+  features.forEach(feature => {
+    featuresDict[feature.properties.id] = feature
+  })
+
+  metaClusters.forEach((cluster) => {
+    if (cluster.leaves) {
+      cluster.leaves.forEach(leaf => {
+        const id = leaf
+        featuresDict[id].properties.cluster_id = cluster.cluster_id
+      })
+    }
+  })
+
+  const clusterPoints = Object.keys(featuresDict).map(id => {
+    const feature = featuresDict[id]
+    return feature
+  })
+
+  return clusterPoints
+}
+
+const getClustersDbscan = (features) => {
+  console.log(features.length, 'features')
+  const t = performance.now()
+  const clusteredFeatures = turf.clustersDbscan(turf.featureCollection(
+    features.slice(0, 10000)
+  ), 6)
+  console.log('done in', performance.now() - t)
+  const noise = clusteredFeatures.features.filter(p => p.properties.dbscan === 'noise')
+  console.log('noise:', noise.length)
+  return clusteredFeatures.features.map(f => {
+    return {
+      ...f,
+      properties: {
+        ...f.properties,
+        cluster_id: f.properties.cluster
+      }
+    }
+  })
+}
+
+const getClusters = (features) => {
+  const clustersDict = {}
+  features.forEach(f => {
+    if (f.properties.cluster_id) {
+      if (!clustersDict[f.properties.cluster_id]) {
+        clustersDict[f.properties.cluster_id] = {
+          point_count: 0,
+          children: []
+        }
+      }
+      clustersDict[f.properties.cluster_id].point_count++
+      clustersDict[f.properties.cluster_id].children.push(f.properties.id)
+    }
+  })
+  const clusters = Object.keys(clustersDict).map(id => {
+    const feature = clustersDict[id]
+    return feature
+  })
+  return clusters
+}
+
 d3.csv(`../in/umap/${umap}.csv`)
   .then(data => {
     let features = data.map(p => {
@@ -51,43 +205,21 @@ d3.csv(`../in/umap/${umap}.csv`)
     const maxY = d3.max(features, d => d.geometry.coordinates[1])
 
     // clusters
-    const index = new Supercluster({
-      radius: 2,
-      maxZoom: 16
-    })
-    index.load(features)
-    let clusters = index.getClusters([minX, minY, maxX, maxY], 2)
-
-    clusters = clusters.map(cluster => {
-      if (!cluster.properties.cluster_id) return cluster
-      return {
-        ...cluster,
-        leaves: index.getLeaves(cluster.properties.cluster_id, Infinity).map(l => l.properties.id)
-      }
-    })
-
-    // features = features.map(feature => {
-    //   const cluster = clusters.find(cluster => {
-    //     if (cluster.leaves === undefined) {
-    //       return undefined
-    //     }
-    //     return cluster.leaves.find(l => l === feature.properties.id)
-    //   })
-    //   return {
-    //     ...feature,
-    //     properties: {
-    //       ...feature.properties,
-    //       clusterId: (cluster) ? cluster.id : undefined,
-    //     }
-    //   }
-    // })
-
-    console.log(features)
-    console.log('Num clusters:', clusters.length)
+    const bbox = [minX, minY, maxX, maxY]
+    console.log(bbox)
+    const clusteredFeatures = getClustersSupercluster(
+      features,
+      // features.slice(0, 9999),
+    bbox)
+    // const clusteredFeatures = getClustersDbscan(features)
+    console.log(clusteredFeatures)
+    const clusters = getClusters(clusteredFeatures)
     console.log(clusters)
+    console.log(d3.mean(clusters.map(c => c.point_count)))
+  
 
     const x = d3.scaleLinear()
-      .domain([minX, maxX])
+      .domain([5, 6])
       .range([ 0, width ])
   
     svg.append('g')
@@ -95,63 +227,63 @@ d3.csv(`../in/umap/${umap}.csv`)
       .call(d3.axisBottom(x))
 
     const y = d3.scaleLinear()
-      .domain([minY, maxY])
+      .domain([-1, 1])
       .range([0, height])
 
     svg.append('g')
       .call(d3.axisLeft(y))
 
 
-    // const colors = {}
-    // const getColor = (id) => {
-    //   if (id === undefined) return '#ff0000'
-    //   if (colors[id]) {
-    //     return colors[id]
-    //   }
-    //   const rd = () => Math.floor(Math.random() * 255)
-    //   colors[id] = `rgb(${rd()}, ${rd()}, ${rd()})`
-    //   return colors[id]
-    // }
+    const colors = {}
+    const getColor = (id) => {
+      if (id === undefined) return '#aaa'
+      if (colors[id]) {
+        return colors[id]
+      }
+      const rd = () => Math.floor(Math.random() * 255)
+      colors[id] = `rgb(${rd()}, ${rd()}, ${rd()})`
+      return colors[id]
+    }
     svg.append('g')
       .attr('id', 'points')
       .selectAll('dot')
-      .data(features)
+      .data(clusteredFeatures)
       .enter()
       .append('circle')
       .attr('cx', (d) => x(d.geometry.coordinates[0]))
       .attr('cy', (d) => y(d.geometry.coordinates[1]))
-      .attr('r', .5)
-      // .style('fill', d => getColor(d.properties.clusterId))
-      .style('fill-opacity', 1)
+      .attr('r', d => (d.properties.cluster_id) ? 1.5 : 1)
+      .style('fill', d => getColor(d.properties.cluster_id))
+      .style('fill-opacity', d => (d.properties.cluster_id) ? 1 : 1)
 
-    svg.append('g')
-      .attr('id', 'clusters')
-      .selectAll('dot')
-      .data(clusters)
-      .enter()
-      .append('circle')
-      .attr('cx', (d) => x(d.geometry.coordinates[0]))
-      .attr('cy', (d) => y(d.geometry.coordinates[1]))
-      .attr('r', (d) => {
-        if (d.properties.point_count === undefined) {
-          return 2
-        }
-        return Math.max(2, d.properties.point_count/100)
-      })
-      .style('fill', d => (d.properties.point_count === undefined) ? '#ff0000' : '#fa5412')
-      .style('stroke', d => (d.properties.point_count === undefined) ? '#ff0000' : '#fa5412')
-      .style('fill-opacity', .3)
-      .style('stroke-opacity', .6)
-      .on('mouseenter', d => {
-        // console.log(d)
-        document.getElementById('info').innerText = d.properties.point_count
-      })
-      .on('click', d => {
-        const leaves = index.getLeaves(d.properties.cluster_id, Infinity)
-        console.log(leaves)
-        console.log(leaves.map(d => d.properties.id))
+    // svg.append('g')
+    //   .attr('id', 'clusters')
+    //   .selectAll('dot')
+    //   .data(clusters)
+    //   .enter()
+    //   .append('circle')
+    //   .attr('cx', (d) => x(d.geometry.coordinates[0]))
+    //   .attr('cy', (d) => y(d.geometry.coordinates[1]))
+    //   .attr('r', (d) => {
+    //     if (d.properties.point_count === undefined) {
+    //       return 2
+    //     }
+    //     return Math.max(2, d.properties.point_count/100)
+    //   })
+    //   .style('fill', d => (d.properties.point_count === undefined) ? '#ff0000' : '#fa5412')
+    //   .style('stroke', d => (d.properties.point_count === undefined) ? '#ff0000' : '#fa5412')
+    //   .style('fill-opacity', .3)
+    //   .style('stroke-opacity', .6)
+    //   .on('mouseenter', d => {
+    //     // console.log(d)
+    //     document.getElementById('info').innerText = d.properties.point_count
+    //   })
+    //   .on('click', d => {
+    //     // const leaves = index.getLeaves(d.properties.cluster_id, Infinity)
+    //     // console.log(leaves)
+    //     // console.log(leaves.map(d => d.properties.id))
 
-      })
+    //   })
 
     
   })
