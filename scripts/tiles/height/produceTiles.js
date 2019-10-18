3f234576f433e38c93607591f0941c1cf4c89c28
j8seangel <jseangel@gmail.com>
2019-10-18 10:12:52 +0200

    move overlap to worker again

--- scripts/tiles/height/produceTiles.js
+++ scripts/tiles/height/produceTiles.js
@@ -1,14 +1,15 @@
 #!/usr/bin/env node
 
 const workerpool = require('workerpool')
-const { ISLANDS } = require('../../constants')
+// const os = require('os')
+// const { ISLANDS } = require('../../constants')
 const fs = require('fs')
 const Jimp = require('jimp')
-const turf = require('@turf/turf')
-const tilebelt = require('@mapbox/tilebelt')
+// const turf = require('@turf/turf')
+// const tilebelt = require('@mapbox/tilebelt')
+// const _ = require('lodash')
 // const { renderTile } = require('./renderTile')
-const _ = require('lodash')
-const { getBboxTiles, heightToRGB, bboxOverlaps } = require('./utils')
+const { getBboxTiles, heightToRGB } = require('./utils')
 const progressBar = require('../../util/progressBar')
 const { performance } = require('perf_hooks')
 const {
@@ -17,7 +18,7 @@ const {
   HEIGHT_EMPTY_TILE
 } = require('../../constants')
 
-const produceHeightBitmap = (islands, bbox, zoomLevel) => {
+const produceHeightBitmap = (bboxIndex, bbox, zoomLevel) => {
   return new Promise((resolve, reject) => {
     if (!zoomLevel) reject('No zoom level passed')
     const pool = workerpool.pool(__dirname + '/worker.js', {
@@ -27,16 +28,13 @@ const produceHeightBitmap = (islands, bbox, zoomLevel) => {
     const pb = progressBar(tiles.length)
     for (let i = 0; i < tiles.length; i++) {
       const tile = tiles[i]
-      const tileBbox = tilebelt.tileToBBOX(tile)
-      const overlappingIslands = _.flatMap(islands, (island) => {
-        const overlaps = bboxOverlaps(tileBbox, island.bbox)
-        return overlaps ? island : []
-      })
-      // renderTile(overlappingIslands, tile, HEIGHT_TILE_SIZE).then(d => {
+
+      // renderTile(bboxIndex, tile, HEIGHT_TILE_SIZE).then(d => {
       //   console.log(d)
       // })
+
       pool
-        .exec('renderTile', [overlappingIslands, tile, HEIGHT_TILE_SIZE])
+        .exec('renderTile', [bboxIndex, tile, HEIGHT_TILE_SIZE])
         .then(() => {
           pb.increment()
           const { pendingTasks, activeTasks } = pool.stats()
@@ -71,16 +69,11 @@ async function generateHeightBitMap(zooms = zoomLevels) {
     const bboxt = performance.now()
     console.log('Starting bbox part ', bboxIndex)
     const bbox = BBOX_CHUNKS[bboxIndex]
-    const islandsPath = ISLANDS.replace('.geo.json', `_${bboxIndex}.geo.json`)
-    const islands = JSON.parse(fs.readFileSync(islandsPath, 'utf-8')).features.map((island) => ({
-      ...island,
-      bbox: turf.bbox(island)
-    }))
     for (let i = 0; i < zooms.length; i++) {
       const zoom = zooms[i]
       const t = performance.now()
       console.log('Starting zoom level', zoom)
-      await produceHeightBitmap(islands, bbox, zoom)
+      await produceHeightBitmap(bboxIndex, bbox, zoom)
       console.log(`${zoom} done in ${(performance.now() - t) / 1000}s`)
     }
     console.log(`Bbox area ${bboxIndex} done in ${(performance.now() - bboxt) / 1000}s`)
