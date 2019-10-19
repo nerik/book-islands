670a4c75cc0e49f308c34e478ca7bf5e1a8bbd86
j8seangel <jseangel@gmail.com>
2019-10-19 12:00:31 +0200

    fix upload height path

--- scripts/upload/uploadHeights.sh
+++ scripts/upload/uploadHeights.sh
@@ -1,1 +1,1 @@
-gsutil -h "Cache-Control:public,max-age=1296000" -h "Content-Encoding:gzip" -m cp -r tiles/heights gs://books-detail-data
+gsutil -h "Cache-Control:public,max-age=1296000" -h "Content-Encoding:gzip" -m cp -r tiles/height gs://books-detail-data
