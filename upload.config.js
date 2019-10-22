4c30f51c94e036caf4b2fb8442d3b42bcdcc4fbe
j8seangel <jseangel@gmail.com>
2019-10-22 17:37:45 +0200

    add bucket cors config

--- upload.config.js
+++ upload.config.js
@@ -1,23 +1,32 @@
+const cacheAge = 1296000
+const bucket = 'cilex-books-map-data'
+const tilesBucket = 'cilex-books-map-tiles'
+
 module.exports = {
   apps : [
     {
+      name: 'set-cors',
+      script: `gsutil cors set bucket-cors.json gs://${bucket}`,
+      autorestart: false,
+    },
+    {
       name: 'upload-data',
-      script: 'gsutil -m cp -r out/search-db/search-db.csv gs://cilex-books-map-data && gsutil -m cp -r out/books gs://cilex-books-map-data && gsutil -m cp -r font-glyphs gs://cilex-books-map-data && gsutil -m cp -r sprites gs://cilex-books-map-data && gsutil -m cp -r tour gs://cilex-books-map-data',
+      script: `gsutil -m cp -r out/search-db/search-db.csv gs://${bucket} && gsutil -m cp -r out/books gs://${bucket} && gsutil -m cp -r font-glyphs gs://${bucket} && gsutil -m cp -r sprites gs://${bucket} && gsutil -m cp -r tour gs://${bucket}`,
       autorestart: false,
     },
     {
       name: 'upload-islands',
-      script: 'gsutil -h "Cache-Control:public,max-age=1296000" -h "Content-Encoding:gzip" -m cp -r tiles/islands gs://cilex-books-map-tiles',
+      script: `gsutil -h "Cache-Control:public,max-age=${cacheAge}" -h "Content-Encoding:gzip" -m cp -r tiles/islands gs://${tilesBucket}`,
       autorestart: false,
     },
     {
       name: 'upload-points',
-      script: 'gsutil -h "Cache-Control:public,max-age=1296000" -h "Content-Encoding:gzip" -m cp -r tiles/points gs://cilex-books-map-tiles',
+      script: `gsutil -h "Cache-Control:public,max-age=${cacheAge}" -h "Content-Encoding:gzip" -m cp -r tiles/points gs://${tilesBucket}`,
       autorestart: false,
     },
     {
       name: 'upload-heights',
-      script: 'gsutil -h "Cache-Control:public,max-age=1296000" -m cp -r tiles/height gs://cilex-books-map-tiles',
+      script: `gsutil -h "Cache-Control:public,max-age=${cacheAge}" -m cp -r tiles/height gs://${tilesBucket}`,
       autorestart: false,
     }
   ],
