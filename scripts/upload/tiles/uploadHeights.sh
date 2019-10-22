4aaa289ccb501ab74cd187158cdf657401140ffe
j8seangel <jseangel@gmail.com>
2019-10-22 09:03:48 +0200

    updates pm2 scripts

--- scripts/upload/tiles/uploadHeights.sh
+++
@@ -1,1 +0,0 @@
-gsutil -h "Cache-Control:public,max-age=1296000" -m cp -r tiles/height gs://cilex-books-map-tiles
---
+++ upload.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  apps : [
+    {
+      name: 'upload-data',
+      script: 'gsutil -m cp -r out/search-db.csv gs://cilex-books-map-data && gsutil -m cp -r out/books gs://cilex-books-map-data',
+      autorestart: false,
+    },
+    {
+      name: 'upload-islands',
+      script: 'gsutil -h "Cache-Control:public,max-age=1296000" -h "Content-Encoding:gzip" -m cp -r tiles/islands gs://cilex-books-map-tiles',
+      autorestart: false,
+    },
+    {
+      name: 'upload-points',
+      script: 'gsutil -h "Cache-Control:public,max-age=1296000" -h "Content-Encoding:gzip" -m cp -r tiles/points gs://cilex-books-map-tiles',
+      autorestart: false,
+    },
+    {
+      name: 'upload-heights',
+      script: 'gsutil -h "Cache-Control:public,max-age=1296000" -m cp -r tiles/height gs://cilex-books-map-tiles',
+      autorestart: false,
+    }
+  ],
+}
