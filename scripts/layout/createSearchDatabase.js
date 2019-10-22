fd053e757b55311af6c501661359a916844b820e
j8seangel <jseangel@gmail.com>
2019-10-22 17:05:35 +0200

    fix upload data script

--- scripts/layout/createSearchDatabase.js
+++ scripts/layout/createSearchDatabase.js
@@ -1,9 +1,13 @@
 #!/usr/bin/env node
 const fs = require('fs')
+const _ = require('lodash')
 const csvStringify = require('csv-stringify/lib/sync')
 
 const {
-  TERRITORY_LABELS, SEARCH_DB
+  TERRITORY_LABELS,
+  SEARCH_DB,
+  SEARCH_DB_RANKED,
+  SEARCH_DB_RANKED_LIMIT
 } = require('../constants')
 
 const authors = JSON.parse(fs.readFileSync(TERRITORY_LABELS, 'utf-8')).features
@@ -14,21 +18,34 @@ const trunc = (n) => {
   return Math.round(n * mult) / mult
 }
 
-const authorsRecords = [['author', 'lng', 'lat']].concat(
-  authors
-    .filter(author => author.properties.rank < 4)
-    .map(author => {
-      return [
-        author.properties.id,
-        trunc(author.geometry.coordinates[0]),
-        trunc(author.geometry.coordinates[1])
-      ]
-    })
+const parseAuthor = (author) => {
+  return [
+    author.properties.id,
+    author.properties.rank,
+    trunc(author.geometry.coordinates[0]),
+    trunc(author.geometry.coordinates[1])
+  ]
+}
+
+const csvFields = ['author', 'rank', 'lng', 'lat']
+
+const authorsRecords = [csvFields].concat(
+  authors.filter((author) => author.properties.rank < 4).map(parseAuthor)
 )
 
+const rankedAuthorRecords = [csvFields].concat(
+  _.sortBy(authors, 'author.properties.rank')
+    .slice(0, SEARCH_DB_RANKED_LIMIT)
+    .map(parseAuthor)
+)
+
+console.log(rankedAuthorRecords.length, 'records')
 console.log(authorsRecords.length, 'records')
 
 const csv = csvStringify(authorsRecords)
+const csvRanked = csvStringify(rankedAuthorRecords)
 fs.writeFileSync(SEARCH_DB, csv)
+fs.writeFileSync(SEARCH_DB_RANKED, csvRanked)
 
 console.log('Wrote', SEARCH_DB)
+console.log('Wrote', SEARCH_DB_RANKED)
