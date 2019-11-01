43d431f290fd67afd9b0fef144d799cbaae79635
nerik <erik.escoffier@gmail.com>
2019-11-01 15:26:41 +0200

    fetch umap scripts

---
+++ scripts/umap/fetchUmapByCat.sh
@@ -0,0 +1,16 @@
+# gsutil cp gs://cilex-books/UMAPS_142k/UMAP_arts_and_entertainment_map.csv in/umap/cat/UMAP_arts_and_entertainment_map.csv
+
+gsutil cp gs://cilex-books/UMAPS_142k/UMAP_people_and_society_map.csv in/umap/cat/UMAP_people_and_society_map.csv
+gsutil cp gs://cilex-books/UMAPS_142k/UMAP_education_map.csv in/umap/cat/UMAP_education_map.csv
+gsutil cp gs://cilex-books/UMAPS_142k/UMAP_religion_and_belief_map.csv in/umap/cat/UMAP_religion_and_belief_map.csv
+gsutil cp gs://cilex-books/UMAPS_142k/UMAP_novels_map.csv in/umap/cat/UMAP_novels_map.csv
+gsutil cp gs://cilex-books/UMAPS_142k/UMAP_science_map.csv in/umap/cat/UMAP_science_map.csv
+gsutil cp gs://cilex-books/UMAPS_142k/UMAP_philosophy_map.csv in/umap/cat/UMAP_philosophy_map.csv
+gsutil cp gs://cilex-books/UMAPS_142k/UMAP_poetry_map.csv in/umap/cat/UMAP_poetry_map.csv
+gsutil cp gs://cilex-books/UMAPS_142k/UMAP_law_and_government_map.csv in/umap/cat/UMAP_law_and_government_map.csv
+gsutil cp gs://cilex-books/UMAPS_142k/UMAP_hobbies_map.csv in/umap/cat/UMAP_hobbies_map.csv
+gsutil cp gs://cilex-books/UMAPS_142k/UMAP_news_map.csv in/umap/cat/UMAP_news_map.csv
+gsutil cp gs://cilex-books/UMAPS_142k/UMAP_children_literature_map.csv in/umap/cat/UMAP_children_literature_map.csv
+gsutil cp gs://cilex-books/UMAPS_142k/UMAP_classics_map.csv in/umap/cat/UMAP_classics_map.csv
+gsutil cp gs://cilex-books/UMAPS_142k/UMAP_arts_and_entertainment_map.csv in/umap/cat/UMAP_arts_and_entertainment_map.csv
+gsutil cp gs://cilex-books/UMAPS_142k/UMAP_history_map.csv in/umap/cat/UMAP_history_map.csv
