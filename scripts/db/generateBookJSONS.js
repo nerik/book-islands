20344418b04969b16da484dcc9a9289ba9870b82
j8seangel <jseangel@gmail.com>
2019-11-26 13:47:34 +0100

    fix books json generation

--- scripts/db/generateBookJSONS.js
+++ scripts/db/generateBookJSONS.js
@@ -8,7 +8,7 @@ const { BOOKS_DB, BOOKS_DB_TABLE, BOOKS_JSON } = require('../constants')
 const sqlite3 = require('sqlite3').verbose()
 const db = new sqlite3.Database(BOOKS_DB, sqlite3.OPEN_READONLY, (err) => {
   if (err) console.log(err)
-  db.all(`SELECT * FROM ${BOOKS_DB_TABLE}`, (err, rows) => {
+  db.all(`SELECT *, id || '' as id FROM ${BOOKS_DB_TABLE}`, (err, rows) => {
     const pb = progressBar(rows.length)
     if (err) console.log(err)
     rows.forEach((row) => {
