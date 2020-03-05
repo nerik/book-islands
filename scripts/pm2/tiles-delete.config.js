267daea6029f358e70905e4c70af03e448ec7011
j8seangel <jseangel@gmail.com>
2020-03-05 15:04:39 +0100

    update pm2 delete script

--- scripts/pm2/tiles-delete.config.js
+++ scripts/pm2/tiles-delete.config.js
@@ -3,18 +3,8 @@ const { STORAGE_BUCKET_TILES, VERSION } = require('../constants')
 module.exports = {
   apps: [
     {
-      name: 'delete-islands',
-      script: `gsutil -m rm -r gs://${STORAGE_BUCKET_TILES}/islands${VERSION}`,
-      autorestart: false,
-    },
-    {
-      name: 'delete-points',
-      script: `gsutil -m rm -r gs://${STORAGE_BUCKET_TILES}/points${VERSION}`,
-      autorestart: false,
-    },
-    {
-      name: 'delete-territories',
-      script: `gsutil -m rm -r gs://${STORAGE_BUCKET_TILES}/territories${VERSION}`,
+      name: 'delete-vectortiles',
+      script: `gsutil -m rm -r gs://${STORAGE_BUCKET_TILES}/allvector${VERSION}`,
       autorestart: false,
     },
     {
