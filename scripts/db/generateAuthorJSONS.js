e38db2362297366180d5b1e3f4b335fc2d51a818
j8seangel <jseangel@gmail.com>
2020-03-05 16:36:26 +0100

    fix concurrency for authors json generation

--- scripts/db/generateAuthorJSONS.js
+++ scripts/db/generateAuthorJSONS.js
@@ -3,12 +3,14 @@
 const workerpool = require('workerpool')
 const fs = require('fs')
 const Database = require('sqlite-async')
+const getAuthorInfo = require('./utils/fetchAuthorInfo')
 
 const kebabCase = require('lodash/kebabCase')
 const { BOOKS_DB, BOOKS_DB_TABLE, AUTHORS_JSON } = require('../constants')
 
-const MAX_WORKERS = 20
+const MAX_WORKERS = 5
 const INDEX_START = 0
+const USE_WORKER = true
 
 var pool = workerpool.pool(__dirname + '/workers/getAuthorInfo.js', {
   maxWorkers: MAX_WORKERS,
@@ -25,27 +27,44 @@ const generateAuthorJsons = async () => {
 
   for (let i = INDEX_START; i < rows.length; i++) {
     const { author } = rows[i]
-    pool
-      .proxy()
-      .then((worker) => {
-        return worker.getAuthorInfo(author)
-      })
-      .then((authorInfo) => {
+    if (USE_WORKER) {
+      pool
+        .proxy()
+        .then((worker) => {
+          return worker.getAuthorInfo(author)
+        })
+        .then((authorInfo) => {
+          console.log(`✅  ${author}`)
+          fs.writeFileSync(`${AUTHORS_JSON}/${kebabCase(author)}.json`, JSON.stringify(authorInfo))
+        })
+        .catch((err) => {
+          console.log(`❌  ${author}`)
+          authorsWithErrors.push(err.message)
+        })
+        .then(() => {
+          const { pendingTasks, activeTasks } = pool.stats()
+          if (pendingTasks === 0 && activeTasks === 0) {
+            fs.writeFileSync(`out/author-errors.json`, JSON.stringify(authorsWithErrors))
+            console.log('Finished! see author with erros in out/author-errors.json')
+            pool.terminate()
+          } else if ((pendingTasks + activeTasks) % MAX_WORKERS === 0) {
+            console.log('Pending tasks', pendingTasks + activeTasks)
+          }
+        })
+    } else {
+      try {
+        const authorInfo = await getAuthorInfo(author)
         fs.writeFileSync(`${AUTHORS_JSON}/${kebabCase(author)}.json`, JSON.stringify(authorInfo))
-      })
-      .catch((err) => {
-        authorsWithErrors.push(err.message)
-      })
-      .then(() => {
-        const { pendingTasks, activeTasks } = pool.stats()
-        if (pendingTasks === 0 && activeTasks === 0) {
-          fs.writeFileSync(`out/author-errors.json`, JSON.stringify(authorsWithErrors))
-          console.log('Finished! see author with erros in out/author-errors.json')
-          pool.terminate()
-        } else if ((pendingTasks + activeTasks) % MAX_WORKERS === 0) {
-          console.log('Pending tasks', pendingTasks + activeTasks)
-        }
-      })
+        console.log(`✅  ${author}`)
+      } catch {
+        console.log(`❌  ${author}`)
+        authorsWithErrors.push(author)
+      }
+    }
+  }
+  if (!USE_WORKER) {
+    fs.writeFileSync(`out/author-errors.json`, JSON.stringify(authorsWithErrors))
+    console.log('Finished! see author with erros in out/author-errors.json')
   }
 }
 
---
+++ scripts/db/utils/fetchAuthorInfo.js
@@ -0,0 +1,100 @@
+require('dotenv').config()
+const rp = require('request-promise')
+const $ = require('cheerio')
+
+const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY
+
+const FETCH_WIKIPEDIA_DATA = true
+const DEBUG = false
+
+function cleanText(text) {
+  if (!text) return ''
+  const removeCharactersRegex = /\[.*?\]|\(.*?\)/g
+  const insertSpaceBetweenMayRegex = /[^- a-z][A-Z]/g
+  return text
+    .replace(/\n/g, ' ')
+    .replace(removeCharactersRegex, ' ')
+    .replace(insertSpaceBetweenMayRegex, (text) => `${text[0]} ${text[1]}`)
+    .replace(/ {2}/g, ' ')
+    .trim()
+}
+
+async function getAuthorInfo(author) {
+  const apiUrl = 'https://kgsearch.googleapis.com/v1/entities:search'
+  const encodedAuthor = encodeURIComponent(author)
+  const uri = `${apiUrl}?query=${encodedAuthor}&key=${GOOGLE_API_KEY}&limit=1&indent=True&types=Person`
+  try {
+    const { itemListElement } = await rp({ uri, json: true })
+    if (itemListElement.length) {
+      const { name, image, url, detailedDescription } = itemListElement[0].result
+      const authorInfo = {
+        id: author,
+        name,
+        url,
+        ...(image && image.contentUrl && { image: image.contentUrl }),
+        bio: detailedDescription && detailedDescription.articleBody,
+      }
+      if (FETCH_WIKIPEDIA_DATA && name) {
+        try {
+          const url = `https://en.wikipedia.org/wiki/${name}`
+          const html = await rp(url, { followAllRedirects: true })
+          const tableRows = $('.infobox.vcard', html)
+            .find('tbody > tr')
+            .toArray()
+            .filter((tr) => tr.firstChild.attribs.scope === 'row')
+          const bornRow = tableRows.length
+            ? tableRows.find((row) =>
+                $(row, html)
+                  .children()
+                  .first()
+                  .text()
+                  .toUpperCase()
+                  .includes('BORN')
+              )
+            : null
+          if (bornRow) {
+            const bornText = cleanText($(bornRow.lastChild, html).text())
+            if (bornText) {
+              authorInfo.born = bornText
+            }
+          }
+          const deathRow = tableRows.length
+            ? tableRows.find((row) =>
+                $(row, html)
+                  .children()
+                  .first()
+                  .text()
+                  .toUpperCase()
+                  .includes('DIED')
+              )
+            : null
+          if (deathRow) {
+            const deathText = cleanText($(deathRow.lastChild, html).text())
+            if (deathText) {
+              authorInfo.death = deathText
+            }
+          }
+        } catch (e) {
+          if (DEBUG) {
+            console.error(e)
+            console.warn(`Error fetching in wikipedia for ${author}`)
+          }
+        }
+      }
+      return authorInfo
+    } else {
+      const error = `No knolegde graph data for author ${author}`
+      if (DEBUG) {
+        console.warn(error)
+      }
+      throw new Error(error)
+    }
+  } catch (e) {
+    if (DEBUG) {
+      console.warn(`Error fetching knolegde graph for author ${author}`, e.message)
+    }
+    throw new Error(author)
+  }
+}
+
+module.exports = getAuthorInfo
