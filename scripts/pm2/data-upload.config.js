88be1a4ccf25ea1b193afa999e0cdf999b8b99b0
j8seangel <jseangel@gmail.com>
2020-03-06 11:33:49 +0100

    gzip search csvs

--- scripts/pm2/data-upload.config.js
+++ scripts/pm2/data-upload.config.js
@@ -9,7 +9,7 @@ module.exports = {
     },
     {
       name: 'upload-data',
-      script: `gsutil -m cp -r out/search-db gs://${STORAGE_BUCKET_DATA} && gsutil -m cp -r out/authors gs://${STORAGE_BUCKET_DATA} && gsutil -m cp -r out/books gs://${STORAGE_BUCKET_DATA} && gsutil -m cp -r font-glyphs gs://${STORAGE_BUCKET_DATA} && gsutil -m cp -r sprite gs://${STORAGE_BUCKET_DATA} && gsutil -m cp -r tour gs://${STORAGE_BUCKET_DATA}`,
+      script: `gzip -r out/search-db && gsutil -h "Content-Encoding:gzip" -m cp -r out/search-db gs://${STORAGE_BUCKET_DATA} && gsutil -m cp -r out/authors gs://${STORAGE_BUCKET_DATA} && gsutil -m cp -r out/books gs://${STORAGE_BUCKET_DATA} && gsutil -m cp -r font-glyphs gs://${STORAGE_BUCKET_DATA} && gsutil -m cp -r sprite gs://${STORAGE_BUCKET_DATA} && gsutil -m cp -r tour gs://${STORAGE_BUCKET_DATA}`,
       autorestart: false,
     },
   ],
